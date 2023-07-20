4704beeab0849abe4a1ac9dd1a06ff00f9c75d22
yin72257 <ruohangyin@gmail.com>
2023-07-20 12:54:41 +0800

    relocated header

--- 02-starting-setup/src/App.js
+++ 02-starting-setup/src/App.js
@@ -1,41 +1,37 @@
-import keyConceptsImage from './assets/images/key-concepts.png';
-import componentsImage from './assets/images/components.png';
-import stateImage from './assets/images/state.png';
-import eventsImage from './assets/images/events.png';
-import ConceptsList from './components/ConceptsList';
+import componentsImage from "./assets/images/components.png";
+import stateImage from "./assets/images/state.png";
+import eventsImage from "./assets/images/events.png";
+import ConceptsList from "./components/ConceptsList";
+import Header from "./components/Header";
 
 const concepts = [
-  {
-    title: 'Components',
-    image: componentsImage,
-    description:
-      'Components let you split the UI into independent, reusable pieces, and think about each piece in isolation. Components can receive data via props, and they can render dynamic output using JSX.',
-  },
-  {
-    title: 'State',
-    image: stateImage,
-    description:
-      'State is data that may change over time. As it changes, the UI should be updated to reflect the updated data. Each component can maintain its own state and multiple components can share state.',
-  },
-  {
-    title: 'Events',
-    image: eventsImage,
-    description:
-      'Event handlers are added via props to (built-in) components. You pass functions as values to such event handlers to control which functions gets executed for which event.',
-  },
+	{
+		title: "Components",
+		image: componentsImage,
+		description:
+			"Components let you split the UI into independent, reusable pieces, and think about each piece in isolation. Components can receive data via props, and they can render dynamic output using JSX.",
+	},
+	{
+		title: "State",
+		image: stateImage,
+		description:
+			"State is data that may change over time. As it changes, the UI should be updated to reflect the updated data. Each component can maintain its own state and multiple components can share state.",
+	},
+	{
+		title: "Events",
+		image: eventsImage,
+		description:
+			"Event handlers are added via props to (built-in) components. You pass functions as values to such event handlers to control which functions gets executed for which event.",
+	},
 ];
 
 function App() {
-  return (
-    <div>
-      <header>
-        <img src={keyConceptsImage} alt="Medal badge with a star" />
-        <h1>Key React Concepts</h1>
-        <p>Selected key React concepts you should know about</p>
-      </header>
-      <ConceptsList concepts={concepts}></ConceptsList>
-    </div>
-  );
+	return (
+		<div>
+			<Header />
+			<ConceptsList concepts={concepts}></ConceptsList>
+		</div>
+	);
 }
 
 export default App;
